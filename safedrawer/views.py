8a39fcd517d65ca0a6fa544730f30a9422203117
Flora Xue <xuezhenruo@berkeley.edu>
2016-09-23 03:07:58 -0700

    implement twilio codes

--- safedrawer/views.py
+++ safedrawer/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from safedrawer.common import *
+
 # Create your views here.
+@api_view(['POST'])
+def alert(request):
+	if request.method == 'POST':
+		print("data", request.data)
+		if test_sms():
+			return Response("Test message sent.", status=status.HTTP_202_ACCEPTED)
+		else:
+			return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
--- serverbackend/urls.py
+++ serverbackend/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
+# Server address: y-team.herokuapp.com
+
 from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^alert/', safedrawer.views.alert),
 ]
