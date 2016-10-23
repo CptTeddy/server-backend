69bd0a0609633e7d9b98e14296cd77857cdef8b8
Flora Xue <xuezhenruo@berkeley.edu>
2016-10-23 03:10:54 -0700

    change phone number to Chinese number

--- safedrawer/common.py
+++ safedrawer/common.py
@@ -9,7 +9,8 @@ tclient = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 
 
 def test_sms():
-    return send_sms("+15107016380", "this is a test")
+    # return send_sms("+15107016380", "this is a test")
+    return send_sms("+8618917094945", "this is a test")
 
 def send_sms(number, message):
     global twilio_client
